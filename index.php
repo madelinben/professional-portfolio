a47b3b7e8a00eb4e35138eb695a6678e69907710
madelinben <ben.madelin@ntlworld.com>
2021-02-02 04:00:05 +0000

    Setup repo directory and initial page.

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF-8">
+
+    <!-- PAGE TITLE -->
+    <title>Professional Portfolio</title>
+
+    <!-- SITE META DATA -->
+    <meta name="keywords" content="professional, portfolio, software engineering, developer">
+    <meta name="description" content="Professional portfolio site to showcase personal projects and achievements.">
+    <meta name="author" content="Ben Madelin">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- SITE RESOURCES -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700" rel="stylesheet" type="text/css">
+    <!--    <link href="https://fonts.googleapis.com/css?family=Poppins:wght@400;700&display=swap" rel="stylesheet" type="text/css">-->
+    <script src="https://kit.fontawesome.com/c642229718.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="icon" type="image/png" href="img\favicon.png">
+
+    <!-- PAGE STYLING -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <!-- PAGE CONTAINER -->
+    <div class="wrapper">
+
+        <!-- NAVBAR -->
+        <header>
+            <img src="img\portfolio-logo.png" class="header-logo" alt="Portfolio Logo">
+        </header>
+
+        <!-- CONTENT -->
+        <div class="content-container">
+
+
+
+        </div>
+
+        <!-- FOOTER -->
+        <footer>
+            <a href="https://github.com/madelinben/"><i class="footer-credit">~ Ben Madelin ~</i></a>
+        </footer>
+    </div>
+</body>
+
+</html>
