0126f28509d2f02139a9b0d57a0fd211d4a5a273
madelinben <ben.madelin@ntlworld.com>
2022-02-25 00:40:11 +0000

    Implemented project page and overview section.

---
+++ client/src/components/ProjectOverview/ProjectOverview.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
+
+import SectionTitle from '../SectionTitle/SectionTitle';
+
+import projectData from '../../assets/data/project';
+import './ProjectOverview.css';
+
+function ProjectOverview() {
+    return (
+        <section id='project'>
+            <SectionTitle title={'Recent Activity'} />
+
+            {projectData && projectData.slice(0,3).map((project, index) => (
+                <div className='project-wrapper'>
+                    <div key={index} className='project-container animated-border'>
+                        <div className='project-image'>
+                            <Link to={project.path.demo}>
+                                <div className='image-wrapper'>
+                                    <div className='image-border'></div>
+                                    <div className='image-cover'></div>
+                                    <img src={project.img} alt='' loading='lazy'></img>
+                                </div>
+                            </Link>
+                        </div>
+
+                        <div className='project-content'>
+                            <p className='project-title'>
+                                <span>{project.title}</span>
+                                <span className='project-link'>
+                                    <a href={project.path.git}><FaIcons.FaGithub /></a>
+                                    <Link to={project.path.demo}><FaIcons.FaExternalLinkAlt /></Link>
+                                </span>
+                            </p>
+                            <hr />
+                            <p className='project-description'>
+                                {project.desc}
+                            </p>
+                            <p className='project-tech-stack'>
+                                {project.techStack.map((tech, index) => (
+                                    <span key={index}>
+                                        <a href={tech.src}>{tech.title}</a>
+                                    </span>
+                                ))}
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+            ))}
+
+            <button><Link to='/project'>See More of My Work</Link></button>
+        </section>
+    );
+}
+
+export default ProjectOverview;
