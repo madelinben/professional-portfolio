eeacf6787c28350bb9fc920bad63140f1f72a7c1
madelinben <ben.madelin@ntlworld.com>
2022-12-08 19:25:26 +0000

    Implemented localstorage and theme provider.

--- client/src/App.js
+++ client/src/App.js
@@ -14,41 +14,41 @@ import './assets/css/Border.css';
 import './assets/css/Bubble.css';
 
 // Components
-import Navbar from './components/Navbar/Navbar';
+import Setting from './components/Sidebar/Setting';
+import Navigation from './components/Sidebar/Navigation';
+import Social from './components/Sidebar/Social';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import Hero from './components/Hero/Hero';
 
 // Routes
-import Profile from './routes/Profile/Profile';
+import Profile from './routes/Profile/Profile.js';
 import Project from './routes/Project/Project.js';
+import SelectedProject from './routes/SelectedProject/SelectedProject.js';
 import Contact from './routes/Contact/Contact.js';
 // import Blog from './routes/Blog/Blog';
-import NotFound from './routes/NotFound/NotFound';
+import NotFound from './routes/NotFound/NotFound.js';
 
 function App() {
-    const [theme, setTheme] = useLocalStorage('theme', null);
-
-    useEffect(() => {
-        if (!theme || (theme === 'dark-theme')) {
-            document.body.classList.add('dark-theme');
-        } else {
-            document.body.classList.add('light-theme');
-        }
-    }, []);
-
+    const [theme, setTheme] = useLocalStorage('theme', 'dark');
+    const [paletteIndex, setPaletteIndex] = useLocalStorage('palette', 0);
+    const paletteArray = ['sunset', 'purple', 'moonlight', 'green', 'grey'];
+    
     const toggleTheme = () => {
-        if (document.body.classList.contains('light-theme')) {
-            document.body.classList.remove('light-theme');
-            document.body.classList.add('dark-theme');
-            setTheme('dark-theme');
-        } else {
-            document.body.classList.remove('dark-theme');
-            document.body.classList.add('light-theme');
-            setTheme('light-theme');
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        setTheme(newTheme);
+    };
+    
+    const togglePalette = () => {
+        let newPalette = paletteIndex + 1;
+        if (newPalette > (paletteArray.length - 1)) {
+            newPalette = 0;
         }
+        setPaletteIndex(newPalette);
     };
 
     return (
         <Router>
-            <div id='layout-wrapper' /* data-theme={theme} */>
+            <div id='layout-wrapper' data-theme={theme} data-palette={paletteArray[paletteIndex]}>
                 <div id='bubble-background'>
                     <span></span>
                     <span></span>
@@ -60,21 +60,27 @@ function App() {
                 
                 <header id='header-wrapper'>
                     <a id='skip-nav' href='#content-wrapper'>skip navigation</a>
-                    <div id='nav-wrapper'><Navbar /></div>
+                    <div id='setting-wrapper'><Setting theme={theme} toggleTheme={toggleTheme} togglePalette={togglePalette} /></div>
+                    <div id='nav-wrapper'><Navigation /></div>
+                    <div id='social-wrapper'><Social /></div>
+                    <div id='scroll-wrapper'><ScrollToTop /></div>
                 </header>
 
                 <div id='content-wrapper'>
+                    <Hero />
+                    {/* <pre>{`${paletteIndex} ${paletteArray[paletteIndex]}`}</pre> */}
+
                     <Switch>
                         <Route path="/" exact component={Profile} />
                         <Route path="/project" exact component={Project} />
-                        {/* <Route path="/project/:selected" exact component={SelectedProject} /> */}
+                        <Route path="/project/:selected" exact component={SelectedProject} />
                         <Route path="/contact" exact component={Contact} />
                         <Route path="*" component={NotFound} />
                     </Switch>
 
                     <footer id='footer-wrapper'>
-                            {/* <p>Made with ❤️ by madelinben.</p> */}
-                            <p>&copy; Benjamin Madelin {new Date().getFullYear()}</p>
+                        {/* <p>Made with ❤️ by madelinben.</p> */}
+                        <p>&copy; Benjamin Madelin {new Date().getFullYear()}</p>
                     </footer>
                 </div>
             </div>
